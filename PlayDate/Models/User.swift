694eb8403291389e38e0ea214a2ed0c6e9635702
GusCornejo <gustavoxcornejo@knights.ucf.edu>
2021-11-15 21:35:30 -0500

    User model created

---
+++ PlayDate/Models/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  PlayDate
+//
+//  Created by Denielle Abaquita on 11/8/21.
+//
+
+import UIKit
+
+struct User {
+    
+    let userId: Int
+    let name: String
+    let bio: String
+    let birthday: String
+    let age: Int
+    let pets: [Pet]
+}
