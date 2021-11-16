12b09ecfb31934d9af1852d4803a7a6ef942cf7d
GusCornejo <gustavoxcornejo@knights.ucf.edu>
2021-11-15 21:57:26 -0500

    Pet model created

---
+++ PlayDate/Models/Pet.swift
@@ -0,0 +1,16 @@
+//
+//  Pet.swift
+//  PlayDate
+//
+//  Created by Denielle Abaquita on 11/8/21.
+//
+
+import UIKit
+
+struct Pet {
+    let petId: Int
+    let name: String
+    let bio: String
+    let type: String
+    let sex: String
+}
