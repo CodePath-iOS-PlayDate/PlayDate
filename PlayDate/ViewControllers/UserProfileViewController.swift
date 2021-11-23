7ef69612ec4ce97f2cf93c2d2aa133ce30c6ce6c
Denielle Abaquita <denielle.abaquita@gmail.com>
2021-11-22 19:18:33 -0500

    Completed header view UI

--- PlayDate/ViewControllers/UserProfileViewController.swift
+++ PlayDate/ViewControllers/UserProfileViewController.swift
@@ -18,10 +18,16 @@ class UserProfileViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        // Register all custom cells
+        self.registerCustomCells()
+    }
+    
+    private func registerCustomCells() {
+        tableView.register(UserProfileHeaderCell.self, forCellReuseIdentifier: UserProfileHeaderCell.identifier)
     }
 
-    // MARK: - Table view data source
-
+    // MARK: - Table View Data Source
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 4
     }
@@ -41,59 +47,46 @@ class UserProfileViewController: UITableViewController {
         }
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+        switch indexPath.section {
+        case 0:
+            guard let cell = tableView.dequeueReusableCell(
+                withIdentifier: UserProfileHeaderCell.identifier,
+                for: indexPath
+            ) as? UserProfileHeaderCell else {
+                return UITableViewCell()
+            }
+            cell.isUserInteractionEnabled = false
+            return cell
+        case 1:
+            break
+        case 2:
+            break
+        case 3:
+            break
+        default:
+            break
+        }
+        return UITableViewCell()
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.section {
+        case 0:
+            return view.bounds.height / 1.8
+        case 1:
+            break
+        case 2:
+            break
+        case 3:
+            break
+        default:
+            break
+        }
+        return view.bounds.height / 7
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- PlayDate/Views/UserProfileCells/UserProfileHeaderCell.swift
+++ PlayDate/Views/UserProfileCells/UserProfileHeaderCell.swift
@@ -8,15 +8,125 @@
 import UIKit
 
 class UserProfileHeaderCell: UITableViewCell {
+    
+    static let identifier = "UserProfileHeaderCell"
+    
+    private let verticalStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .equalCentering
+        return stackView
+    }()
+    
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
+    
+    private let buttonStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        stackView.spacing = 33
+        return stackView
+    }()
+    
+    private let editButton: UIButton = {
+        var config = UIButton.Configuration.filled()
+        config.buttonSize = .large
+        config.preferredSymbolConfigurationForImage = UIImage.SymbolConfiguration(font: .systemFont(ofSize: 29), scale: UIImage.SymbolScale.large)
+        config.background.cornerRadius = 80
+        config.cornerStyle = .fixed
+        
+        let button = UIButton(configuration: config, primaryAction: nil)
+        button.setImage(UIImage(systemName: "square.and.pencil"), for: .normal)
+        
+        return button
+    }()
+    
+    private let cameraButton: UIButton = {
+        var config = UIButton.Configuration.filled()
+        config.buttonSize = .large
+        config.preferredSymbolConfigurationForImage = UIImage.SymbolConfiguration(font: .systemFont(ofSize: 29), scale: UIImage.SymbolScale.large)
+        config.background.cornerRadius = 80
+        config.cornerStyle = .fixed
+        
+        let button = UIButton(configuration: config, primaryAction: nil)
+        button.setImage(UIImage(systemName: "camera.fill"), for: .normal)
+        
+        return button
+    }()
+    
+    private let signOutButton: UIButton = {
+        var config = UIButton.Configuration.filled()
+        config.buttonSize = .large
+        config.preferredSymbolConfigurationForImage = UIImage.SymbolConfiguration(font: .systemFont(ofSize: 29), scale: UIImage.SymbolScale.large)
+        config.background.cornerRadius = 80
+        config.cornerStyle = .fixed
+        
+        let button = UIButton(configuration: config, primaryAction: nil)
+        button.setImage(UIImage(systemName: "arrow.up.forward.circle.fill"), for: .normal)
+        
+        return button
+    }()
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    // MARK: Initializers
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.contentView.backgroundColor = Colors.bluePurple
+        setupViews()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
     }
+    
+    // MARK: View Setup
+    private func setupViews() {
+        // Stack View
+        self.contentView.addSubview(self.verticalStackView)
+        self.verticalStackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.verticalStackView.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor),
+            self.verticalStackView.centerXAnchor.constraint(equalTo: self.contentView.centerXAnchor),
+            self.verticalStackView.widthAnchor.constraint(equalTo: self.contentView.widthAnchor),
+        ])
+        
+        // Profile Image
+        guard let image = UIImage(named: "LogoMedium") else {
+            print("Error - Could not load profile image")
+            return
+        }
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        imageView.maskCircle(anyImage: image)
+        self.verticalStackView.addArrangedSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: self.contentView.bounds.height)
+        ])
+        
+        // Buttons
+        self.verticalStackView.addArrangedSubview(self.buttonStackView)
+        NSLayoutConstraint.activate([
+            self.buttonStackView.heightAnchor.constraint(equalToConstant: 100),
+            self.buttonStackView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 31),
+            self.buttonStackView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -31),
+        ])
+        
+        self.buttonStackView.addArrangedSubview(editButton)
+        self.buttonStackView.addArrangedSubview(cameraButton)
+        self.buttonStackView.addArrangedSubview(signOutButton)
+    }
+}
+
+extension UIImageView {
+  public func maskCircle(anyImage: UIImage) {
+    self.contentMode = .scaleAspectFill
+    self.layer.cornerRadius = self.frame.size.width / 2
+    self.layer.masksToBounds = false
+    self.clipsToBounds = true
+    self.image = anyImage
+  }
 }
