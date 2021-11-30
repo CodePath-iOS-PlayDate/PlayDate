4b3f8271e357d5ec5d24349423ccd12706b3b715
yupyup <yupyuptempy@yupyups-iMac.local>
2021-11-29 20:53:14 -0800

    Pet  model setters and getters

--- PlayDate/Models/Pet.swift
+++ PlayDate/Models/Pet.swift
@@ -6,12 +6,74 @@
 //
 
 import UIKit
+import FirebaseFirestore
+
+let coll = db.collection("pets")
+
 
 struct Pet {
-    let petId: Int
-    let name: String
-    let bio: String
-    let type: String
-    let sex: String
-    let age: String
+    private var name : String?
+    private var age : Int?
+    private var bio : String?
+    private var type: String?
+    private var sex: String?
+    private var id: String
+    
+    //Create another constructor for the loaded document
+    init(){
+        // generate document with unique id then save THAT as pet id
+        // load pet data to corresponding user pet entry here also.
+        var ref : DocumentReference? = nil
+        
+        ref = coll.addDocument(data:[
+            "name" : " ",
+            "type" : " ",
+            "sex" : " "
+            ])
+        
+        self.id = ref!.documentID
+    }
+    
+    init (id: String){
+        self.id = id
+    }
+    
+    func updateName(name: String){
+        coll.document("\(self.id)").setData(["name": name], merge: true)
+    }
+    
+    func updateType(type: String){
+        coll.document("\(self.id)").setData(["type": type], merge: true)
+    }
+    
+    func updateAge(age: Int){
+        coll.document("\(self.id)").setData(["age": age], merge: true)
+
+    }
+    
+    
+    func updateBio(bio: String){
+        coll.document("\(self.id)").setData(["bio": bio], merge: true)
+
+    }
+    
+    func getID()-> String{
+        return self.id
+    }
+    
+    func getInfo(completion: @escaping (Dictionary<String, Any>?)-> Void ){
+        //var doc : Dictionary<String, Any> = Dictionary<String, Any>()
+        let docRef = coll.document(self.id)
+        
+        docRef.getDocument{(document, error) in
+            if let document = document, document.exists{
+                let data = document.data()! as Dictionary?
+                completion(data)
+            } else {
+                print("problem")
+                completion(nil)
+            }
+        }
+    }
+    
 }
