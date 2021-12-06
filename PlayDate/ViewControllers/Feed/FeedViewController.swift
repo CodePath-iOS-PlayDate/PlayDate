54ec44503096a785926e5573a92d1f965cca776d
Denielle Abaquita <denielle.abaquita@gmail.com>
2021-12-06 14:15:41 -0500

    Added card with swiping left and right gestures

--- PlayDate/ViewControllers/Feed/FeedViewController.swift
+++ PlayDate/ViewControllers/Feed/FeedViewController.swift
@@ -10,37 +10,82 @@ import Shuffle_iOS
 
 class FeedViewController: UIViewController {
     
-    let cardStack = SwipeCardStack()
+    private let cardStack = SwipeCardStack()
+    private let petProfileCardModels = [
+        PetProfile(
+            name: "Sample",
+            age: 1,
+            type: "Dog",
+            image: UIImage(named: "LogoMedium")
+        ),
+        PetProfile(
+            name: "Sample 2",
+            age: 2,
+            type: "Dog",
+            image: UIImage(named: "LogoMedium")
+        ),
+        PetProfile(
+            name: "Sample 3",
+            age: 3,
+            type: "Dog",
+            image: UIImage(named: "LogoMedium")
+        ),
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Feed"
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .white
+        appearance.largeTitleTextAttributes = [.foregroundColor: Colors.blueGreen]
+        appearance.titleTextAttributes = [.foregroundColor: Colors.blueGreen]
+        appearance.shadowColor = .clear
+        self.navigationController?.navigationBar.standardAppearance = appearance
+        self.navigationController?.navigationBar.scrollEdgeAppearance = self.navigationController?.navigationBar.standardAppearance
+        self.navigationController?.navigationBar.layoutIfNeeded()
         
         cardStack.dataSource = self
         cardStack.delegate = self
         view.addSubview(cardStack)
         cardStack.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            cardStack.topAnchor.constraint(equalTo: view.topAnchor),
-            cardStack.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            cardStack.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            cardStack.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            cardStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            cardStack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            cardStack.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            cardStack.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
     }
 }
 
 extension FeedViewController: SwipeCardStackDataSource, SwipeCardStackDelegate {
-    func card(fromImage image: UIImage) -> SwipeCard {
+    func cardStack(_ cardStack: SwipeCardStack, cardForIndexAt index: Int) -> SwipeCard {
         let swipeCard = SwipeCard()
-        swipeCard.largeContentImage = UIImage(named: "LogoMedium")
+        swipeCard.swipeDirections = [.left, .up, .right]
+        // Like / Dislike overlay needs to be add here for each swipe direction
+
+        let petModel = petProfileCardModels[index]
+        swipeCard.content = CardContentView(withImage: petModel.image)
         return swipeCard
     }
     
-    func cardStack(_ cardStack: SwipeCardStack, cardForIndexAt index: Int) -> SwipeCard {
-        return card(fromImage: UIImage())
+    func numberOfCards(in cardStack: SwipeCardStack) -> Int {
+        return petProfileCardModels.count
     }
     
-    func numberOfCards(in cardStack: SwipeCardStack) -> Int {
-        return 1
+    func didSwipeAllCards(_ cardStack: SwipeCardStack) {
+      print("Swiped all cards!")
+    }
+    
+    func cardStack(_ cardStack: SwipeCardStack, didUndoCardAt index: Int, from direction: SwipeDirection) {
+      print("Undo \(direction) swipe on \(petProfileCardModels[index].name)")
+    }
+
+    func cardStack(_ cardStack: SwipeCardStack, didSwipeCardAt index: Int, with direction: SwipeDirection) {
+      print("Swiped \(direction) on \(petProfileCardModels[index].name)")
+    }
+
+    func cardStack(_ cardStack: SwipeCardStack, didSelectCardAt index: Int) {
+      print("Card tapped on \(petProfileCardModels[index].name)")
     }
 }
