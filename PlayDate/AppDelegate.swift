8a5d6930157637d3ddc014afd8ff06284c6674bf
michaelsoffline <cuberomichael13@gmail.com>
2021-12-06 14:40:41 -0500

    Changed back to MainLaunchViewController in App Delegate
    Forgot to change the main screen back to MainLaunchViewController when I pushed my most recent commit. Resolved with this commit.

--- PlayDate/AppDelegate.swift
+++ PlayDate/AppDelegate.swift
@@ -25,9 +25,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // View controllers
         window?.backgroundColor = .systemBackground
         
-        let main = MatchContactInfoViewController()
+        let main = MainLaunchViewController()
         let logoImage = UIImage(named: "LogoMedium")
-        //main.logoImage = logoImage
+        main.logoImage = logoImage
         
         let navVC = UINavigationController(rootViewController: main)
         window?.rootViewController = navVC
